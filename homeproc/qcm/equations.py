d1693f214b6de3721ce79f8d06ec17064438c80d
Paul Iacomi <pauliacomi@users.noreply.github.com>
2022-02-23 10:42:46 +0100

    reverse sauerbrey

--- homeproc/qcm/equations.py
+++ homeproc/qcm/equations.py
@@ -2,6 +2,7 @@ import math
 
 __all__ = [
     'sauerbrey',
+    'reverse_sauerbrey',
 ]
 
 d = 0.51  # electrode diameter, cm
@@ -13,3 +14,8 @@ mu = 3.947e11  # shear modulus AT cut g /cm /s2
 def sauerbrey(dF, F0):
     "Calculate mass (in mg) as a function of resonance frequency and base frequency (in Hz)."
     return (dF * A * math.sqrt(rho * mu) / (-2 * F0**2)) * 1000  # mass, mg
+
+
+def reverse_sauerbrey(dm, F0):
+    "Calculate resonance frequency change (in Hz) as a function of mass (in mg) and base frequency."
+    return (-2 * dm * F0**2) / (A * math.sqrt(rho * mu) * 1000)
