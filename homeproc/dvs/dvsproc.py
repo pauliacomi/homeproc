16385df086f2a8c51437b9bb4ef411d0689cb790
Paul Iacomi <pauliacomi@users.noreply.github.com>
2022-04-05 11:48:14 +0200

    clearer dvs cols

--- homeproc/dvs/dvsproc.py
+++ homeproc/dvs/dvsproc.py
@@ -21,8 +21,6 @@ from ..common import pairwise, plot_transient
 __all__ = [
     'read_dvs_file',
     'get_change_points',
-    'get_change_points_rpt',
-    'get_change_points_rpt_wnd',
     'calc_isotherm_data',
     'trim_meta',
     'get_loading',
@@ -46,7 +44,7 @@ important_meta = {
     "Vapour": "dvs_adsorbate",
     "Vapour Pressure [Torr]": "dvs_p0 [torr]",
     "Control Mode": "dvs_control_mode",
-    "columns": "col"
+    "columns": "columns"
 }
 
 cols = dict(
@@ -76,9 +74,9 @@ cols = dict(
 def read_dvs_file(path, offset=20):
     """Read a DVS 'txt' file and return its metadata and data."""
 
+    # Read metadata
     with open(path, encoding="cp1252") as f:
         f.readline()
-
         dvsinfo = {}
         for i, line in enumerate(f.readlines()):
             key, val = map(str.strip, line.split(':', 1))
@@ -86,62 +84,105 @@ def read_dvs_file(path, offset=20):
             if i > 14:
                 break
 
-    dvsdata = pd.read_csv(path, encoding="cp1252", delimiter="\\t", skiprows=41, engine='python')
+    # Read data
+    dvsdata = pd.read_csv(
+        path,
+        encoding="cp1252",
+        delimiter="\\t",
+        skiprows=41,
+        engine='python',
+    )
 
+    # Add some other metadata
+    # columns
     dvsinfo['columns'] = {k: dvsdata.columns[v] for k, v in cols.items()}
-
+    # creation date
     file_created = dvsinfo['Raw Data File Created'][:19]
     file_created = parser.parse(file_created) + datetime.timedelta(seconds=offset)
+    # Trim unneeded data
+    dvsinfo = trim_meta(dvsinfo)
+    # activation T
+    dvsinfo['activation_temp [C]'] = get_act_T(dvsdata, dvsinfo['columns']['t_heat'])
 
+    # Reindex data
     dvsdata = dvsdata.set_index(
         file_created + pd.to_timedelta(dvsdata[dvsinfo['columns']['time']], unit='min')
     )
-    dvsinfo = trim_meta(dvsinfo)
 
     return dvsinfo, dvsdata
 
 
+def get_act_T(dvsdata, tcol):
+    """Find activation temperature as maximum temperature of data"""
+    return max(dvsdata[tcol])
+
+
 def trim_meta(meta):
+    """Trim metadata to remove unnecessary values."""
     return {important_meta[key]: val for key, val in meta.items() if key in important_meta}
 
 
-def get_change_points_rpt(dvsdata, col, pen=0.5, min_size=2, **kwargs):
+def dvs_plot(dvsinfo, dvsdata):
+    """Plot kinetic DVS data."""
+    fig = plot_transient(
+        dvsdata,
+        dvsinfo['columns']['mass'],
+        dvsinfo['columns']['t_heat'],
+        dvsinfo['columns']['p_abs'],
+        dvsinfo['columns']['p_abs_tgt'],
+    )
+    fig.update_layout(yaxis2_range=[20, 200])  # set T
+    return fig
+
+
+def get_change_points(
+    dvsdata,
+    col,
+    method="window",
+    pen=0.5,
+    width=300,
+    min_size=2,
+    **kwargs,
+):
+    """Find the change points in a data series (usually pressure)."""
 
     datacol = dvsdata[col].fillna(0)
-    algo = rpt.Binseg(model="l2", min_size=min_size, **kwargs).fit(datacol.values)
-    chpoints = algo.predict(pen=pen)
-    rpt.show.display(datacol.values, chpoints, figsize=(17, 6))
+
+    if method == "derivative":
+        datacol = dvsdata[col].fillna(0)
+        diff = np.diff(datacol, prepend=datacol[0])
+        chpoints = np.nonzero(diff)[0]
+        chpoints = np.append(chpoints, [len(datacol) - 1])
+
+        fig, ax = plt.subplots(1, figsize=(17, 6))
+        ax.plot(range(len(datacol)), datacol)
+        colors = cycle(["#4286f4", "#f44174"])
+        for (start, end), col in zip(pairwise(chpoints), colors):
+            ax.axvspan(max(0, start - 0.5), end - 0.5, facecolor=col, alpha=0.2)
+
+    elif method == "window":
+        algo = rpt.Window(model="l1", width=width, **kwargs).fit(datacol.values)
+        chpoints = algo.predict(pen=pen)
+        rpt.show.display(datacol.values, chpoints, figsize=(17, 6))
+
+    elif method == "binary_segment":
+        algo = rpt.Binseg(model="l2", min_size=min_size, **kwargs).fit(datacol.values)
+        chpoints = algo.predict(pen=pen)
+        rpt.show.display(datacol.values, chpoints, figsize=(17, 6))
+
+    else:
+        raise BaseException("Incorrect method.")
 
     return chpoints
 
 
-def get_change_points_rpt_wnd(dvsdata, col, pen=0.5, width=300, **kwargs):
-
-    datacol = dvsdata[col].fillna(0)
-    algo = rpt.Window(model="l1", width=width, **kwargs).fit(datacol.values)
-    chpoints = algo.predict(pen=pen)
-    rpt.show.display(datacol.values, chpoints, figsize=(17, 6))
-
-    return chpoints
-
-
-def get_change_points(dvsdata, col, pen=0.5, width=300, **kwargs):
-
-    datacol = dvsdata[col].fillna(0)
-    diff = np.diff(datacol, prepend=datacol[0])
-    chpoints = np.nonzero(diff)[0]
-    chpoints = np.append(chpoints, [len(datacol) - 1])
-
-    fig, ax = plt.subplots(1, figsize=(17, 6))
-    ax.plot(range(len(datacol)), datacol)
-    colors = cycle(["#4286f4", "#f44174"])
-    for (start, end), col in zip(pairwise(chpoints), colors):
-        ax.axvspan(max(0, start - 0.5), end - 0.5, facecolor=col, alpha=0.2)
-
-    return chpoints
+def get_loading(iso_points, m0, c='loading'):
+    """Find loading by dividing by initial mass"""
+    return (iso_points[c] / m0 - 1)
 
 
 def calc_isotherm_data(dvsdata, pcol, mcol, chpoints, extra_cols=None, offspts=10, meanpts=20):
+    """Select and average points to calculate isotherm data."""
 
     mean = offspts + meanpts
 
@@ -157,28 +198,9 @@ def calc_isotherm_data(dvsdata, pcol, mcol, chpoints, extra_cols=None, offspts=1
     return pd.DataFrame(read_cols)
 
 
-def get_act_T(dvsdata, tcol):
-    return max(dvsdata[tcol])
-
-
 def remove_baseline(iso_points, base_name, folder_path=LP_BASELINES_PTH):
+    """Remove isotherm baseline."""
     base_iso = pgp.isotherm_from_csv(pth.Path(folder_path) / base_name)
     base_iso.convert(pressure_unit='torr')
     adj_p = base_iso.loading_at(iso_points['pressure'], interpolation_type='slinear', interp_fill=0)
     return iso_points['loading'] - adj_p
-
-
-def get_loading(iso_points, m0, c='loading'):
-    return (iso_points[c] / m0 - 1)
-
-
-def dvs_plot(dvsinfo, dvsdata):
-    fig = plot_transient(
-        dvsdata,
-        dvsinfo['col']['mass'],
-        dvsinfo['col']['t_heat'],
-        dvsinfo['col']['p_abs'],
-        dvsinfo['col']['p_abs_tgt'],
-    )
-    fig.update_layout(yaxis2_range=[20, 200])  # set T
-    return fig
