9dd629f4a44aefcdb8b9a8607c2f8561afdb1c6c
Paul Iacomi <pauliacomi@users.noreply.github.com>
2022-05-05 14:49:27 +0200

    add option to specify electrode diameter

--- homeproc/qcm/equations.py
+++ homeproc/qcm/equations.py
@@ -1,4 +1,5 @@
 import math
+import functools
 
 __all__ = [
     'sauerbrey',
@@ -6,16 +7,21 @@ __all__ = [
 ]
 
 d = 0.51  # electrode diameter, cm
-A = math.pi * (d / 2)**2  # electrode area, cm2
 rho = 2.648  # quartz density, g/cm3
 mu = 3.947e11  # shear modulus AT cut g /cm /s2
 
 
-def sauerbrey(dF, F0):
+@functools.cache
+def area(de):
+    """electrode area, cm2"""
+    return math.pi * (de / 2)**2
+
+
+def sauerbrey(dF, F0, de=d):
     "Calculate mass (in mg) as a function of resonance frequency and base frequency (in Hz)."
-    return (dF * A * math.sqrt(rho * mu) / (-2 * F0**2)) * 1000  # mass, mg
+    return (dF * area(de) * math.sqrt(rho * mu) / (-2 * F0**2)) * 1000
 
 
-def reverse_sauerbrey(dm, F0):
+def reverse_sauerbrey(dm, F0, de=d):
     "Calculate resonance frequency change (in Hz) as a function of mass (in mg) and base frequency."
-    return (-2 * dm * F0**2) / (A * math.sqrt(rho * mu) * 1000)
+    return (-2 * dm * F0**2) / (area(de) * math.sqrt(rho * mu) * 1000)
